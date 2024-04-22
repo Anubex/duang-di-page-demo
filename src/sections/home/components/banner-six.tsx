5899bb3578329f7e49dba4bf7f80cdfaffc4ba25
Anubex <louisgodanubex@gmailcom>
2024-04-22 14:43:45 +0700

    update front

--- src/sections/home/components/banner-six.tsx
+++ src/sections/home/components/banner-six.tsx
@@ -3,6 +3,7 @@ import { Button, Link, Typography } from '@mui/material'
 import { validate } from 'numeral'
 import ReactMarkdown from 'react-markdown'
 import { useState } from 'react'
+
 export default function BannerSix() {
   return (
     <>
@@ -20,12 +21,14 @@ export default function BannerSix() {
         <Typography variant="body1">
           ยินดีต้อนรับสู่เว็บไซต์เบอร์ดี เบอร์สวย เบอร์มงคล ซิมเบอร์สวย ซิมเบอร์มงคล ทำนายเบอร์โทรศัพท์ ซิมการ์ดเบอร์โทรศัพท์มือถือ
         </Typography>
-      </div>
-              <div className='App h-screen bg-black
-              flex justify-center items-center'>
+      </div> 
+      
+      <div className=' h-screen bg-blue-200
+              flex justify-center items-center w-4/5 h-24 absolute top-50 right-0'>
+                
               <label className=''>
                 ค้นหาเบอร์
-                <input type='text' className='h-10 w-8'/>
+                <input type='text' className='h-10 w-8'/> 
                 <input type='text' className='h-10 w-8'/>
                 <input type='text' className='h-10 w-8'/>
                 <input type='text' className='h-10 w-8'/>
@@ -37,55 +40,16 @@ export default function BannerSix() {
                 <input type='text' className='h-10 w-8'/>
 
               </label>
-              </div>
-      <div className="container mx-auto pb-[30px]">
-        <div className="flex gap-8 overflow-x-auto p-4 md:grid md:grid-cols-2 md:gap-8 md:whitespace-normal md:p-0">
-          {services.map((serviceData: EServiceT) => (
-            <div
-              className="e-service-card flex flex-shrink-0 flex-col rounded-md"
-              key={`${serviceData.name}-key`}
-            >
-              <div className="hide-with-banner relative">
-                <div className="flex h-full w-full flex-col px-[30px] py-[30px] md:px-[64px] md:py-[64px]">
-                  <div className="z-10 flex-col items-start justify-center px-6">
-                    <div className="z-9 absolute left-0 top-0 h-full w-full rounded-lg bg-gradient-to-b from-white to-[#c6f8d8]" />{' '}
-                    <div className="relative z-10">
-                      <ReactMarkdown
-                        className={
-                          serviceData?.textClassName ??
-                          'prose mb-[14px] w-[64%] max-w-full text-center text-xl font-bold md:w-[56%] md:text-3xl md:leading-[42px]'
-                        }
-                      >
-                        {serviceData.name}
-                      </ReactMarkdown>
-                      <div
-                        className={
-                          serviceData?.buttonClassName ??
-                          'mt-[36px] flex w-[64%] items-center justify-center gap-4 md:w-[56%] 2xl:gap-10'
-                        }
-                      >
-                        <Link
-                          href={serviceData?.to}
-                          component={Button}
-                          underline="none"
-                          className="flex h-[52px] w-[233px] max-w-[233px] items-center justify-center gap-[5px] rounded-[10px] bg-gradient-to-b from-[#28A745] to-[#227041] text-[20px] font-medium !text-white shadow-lg hover:!from-[#28A745]/90 hover:!to-[#227041]/90"
-                        >
-                          {serviceData.button}
-                        </Link>
-                      </div>
-                    </div>
-                    <img
-                      src={serviceData.image}
-                      className="absolute bottom-0 left-0 h-full w-full overflow-hidden rounded-md object-cover opacity-70"
-                      alt={`${serviceData.name}-image-transparent`}
-                    />
-                  </div>
-                </div>
-              </div>
-            </div>
-          ))}
-        </div>
-      </div>
+              
+           </div>
+           <div className=' h-screen bg-white
+              flex justify-center items-center'>
+
+
+
+           </div>
+       
+   
     </>
   )
 }
